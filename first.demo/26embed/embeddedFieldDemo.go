da3081910db6c2b30c8c2694834b0a08b4c37699
wuzheng <17173as@163.com>
2022-09-05 09:00:19 +0800

    云原生训练营，多线程相关练习

--- first.demo/26embed/embeddedFieldDemo.go
+++ first.demo/26embed/embeddedFieldDemo.go
@@ -17,9 +17,22 @@ type t struct {
 	b int
 }
 
+type SecInt int
+
+func (n *SecInt) Add(m int) {
+	*n = *n + SecInt(m)
+}
+
+type I interface {
+	M1()
+	M2()
+}
 type S struct {
 	*MyInt
+	//如果存在相同方法的类型嵌入，直接使用s.Add会有编译错误
+	//*SecInt
 	t
+	I
 	io.Reader
 	s string
 	n int
@@ -44,4 +57,7 @@ func main() {
 	//s.MyInt.Add(5)
 	s.Add(5) // 等同于 s.MyInt.Add(5)
 	fmt.Println(*(s.MyInt))
+	//因为S 没有对应的M1方法，所以会报错
+	//s.M1()
+
 }
