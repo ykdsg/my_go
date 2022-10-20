d75fce87a432ca2b56687783587d333f36accd59
wuzheng <17173as@163.com>
2022-10-20 17:02:52 +0800

    增加类型函数的demo。

---
+++ hz.com/yk/base/goroutine/goroutineDemo1.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"github.com/ykdsg/base/util"
+	"time"
+)
+
+func justPrint() {
+	fmt.Println("I am print------------------")
+}
+func m2() {
+	fmt.Println("m2 start----------------", util.GoID())
+}
+
+func m1() {
+	fmt.Println("m1 start..............", util.GoID())
+	go m2()
+	justPrint()
+	fmt.Println("m1 end..............", util.GoID())
+
+	for i := 0; i < 50000; i++ {
+		if i%500 == 0 {
+			fmt.Println("current i=", i)
+		}
+	}
+}
+
+//从这个例子中可以看到goroutine 没有这么快启动，但也并不是等函数结束的时候才会启动。
+func main() {
+	fmt.Println("main start..............", util.GoID())
+	go m1()
+	time.Sleep(500 * time.Millisecond)
+	fmt.Println("main end..............", util.GoID())
+	time.Sleep(10 * time.Second)
+	fmt.Println("main finish..............", util.GoID())
+}
