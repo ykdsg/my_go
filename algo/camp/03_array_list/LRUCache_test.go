559b22fd2e53f6c5c6933ac708a0b1084a043c66
wuzheng <17173as@163.com>
2022-11-24 10:51:02 +0800

    LRU 相关练习

--- algo/camp/03_array_list/LRUCache_test.go
+++ algo/camp/03_array_list/LRUCache_test.go
@@ -24,3 +24,18 @@ func TestLRUCache_Put(t *testing.T) {
 	ast.Equal(3, cache.Get(3))
 	ast.Equal(4, cache.Get(4))
 }
+
+func TestLRUCache_Put2(t *testing.T) {
+	cache := Constructor(2)
+	cache.Put(1, 1)
+	cache.Put(2, 6)
+	cache.Get(1)
+	cache.Put(1, 5)
+	cache.Put(1, 2)
+	reslut1 := cache.Get(1)
+	ast := assert.New(t)
+	ast.Equal(2, reslut1)
+	reslut2 := cache.Get(2)
+	ast.Equal(6, reslut2)
+
+}
