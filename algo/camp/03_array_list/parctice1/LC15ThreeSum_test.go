4c7369555d07813716e47e523bcdcc924512f233
wuzheng <17173as@163.com>
2022-12-29 10:24:55 +0800

    复习链表相关算法。

--- algo/camp/03_array_list/parctice1/LC15ThreeSum_test.go
+++ algo/camp/03_array_list/parctice1/LC15ThreeSum_test.go
@@ -24,3 +24,11 @@ func TestThreeSum(t *testing.T) {
 	result = threeSum(nums)
 	ast.Equal([][]int{[]int{0, 0, 0}}, result)
 }
+
+func TestThreeSum3(t *testing.T) {
+	ast := assert.New(t)
+
+	nums := []int{-1, 0, 1, 2, -1, -4}
+	result := threeSum3(nums)
+	ast.Equal([][]int{[]int{-1, -1, 2}, []int{-1, 0, 1}}, result)
+}
